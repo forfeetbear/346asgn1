b58e9e5f9b797d539708776a3fde58657a85da82
Matthew Bennett <mabennett@localhost.(none)>
2012-08-17 16:43:33 +1200

    Added the MPInteger Class files and the sample code for main.m also added some code to main.m so I can manually test the functions OR test the decryption

---
+++ MultiPrecisionInteger/MPInteger.h
@@ -0,0 +1,29 @@
+//
+//  MPInteger.h
+//  MultiPrecisionInteger
+//
+//  Created by Matthew Bennett on 8/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPInteger : NSObject
+
+// you will need these for me to test your code:
+-(id) initWithString: (NSString *) x;
+-(NSString *) description;
+
+// your grade will be determined according to
+// 20% for each of the following:
+-(MPInteger *) add: (MPInteger *) x;
+-(MPInteger *) subtract: (MPInteger *) x;
+-(MPInteger *) multiply: (MPInteger *) x;
+-(MPInteger *) divideBy: (MPInteger *) x;
+-(MPInteger *) modulus: (MPInteger *) x;
+
+// you will also need to implement this comparison
+// to decode the secret message:
+-(BOOL) isLessThan: (MPInteger *) x;
+
+@end
---
+++ MultiPrecisionInteger/MPInteger.m
@@ -0,0 +1,13 @@
+//
+//  MPInteger.m
+//  MultiPrecisionInteger
+//
+//  Created by Matthew Bennett on 8/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MPInteger.h"
+
+@implementation MPInteger
+
+@end
