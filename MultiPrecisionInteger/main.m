f611a7aa26fc2e6d28221b751aebe7d17dd66be1
Matthew Bennett <mabennett@localhost.(none)>
2012-08-17 16:15:45 +1200

    Initial commit

---
+++ MultiPrecisionInteger/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MultiPrecisionInteger
+//
+//  Created by Matthew Bennett on 8/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
